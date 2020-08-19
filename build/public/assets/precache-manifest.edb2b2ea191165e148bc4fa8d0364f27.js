f9e03c5d72739731957ed9132ab1a54e6f6eb6a7
Amit Jain <amitjain628@gmail.com>
2020-08-20 00:51:30 +0530

    Adedd babel

---
+++ build/public/assets/precache-manifest.edb2b2ea191165e148bc4fa8d0364f27.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc639b53db2b15cad81c",
+    "url": "/assets/vendors.dc639b53.js"
+  },
+  {
+    "revision": "882802f59d9ee4cd408f",
+    "url": "/assets/home.882802f5.js"
+  },
+  {
+    "revision": "393e48d6807d9341b8f2",
+    "url": "/assets/client.393e48d6.js"
+  },
+  {
+    "revision": "164df16c84e325c68d54298cefc111ce",
+    "url": "/app-shell"
+  }
+];
---
+++ build/public/assets/sw.js
@@ -0,0 +1,44 @@
+importScripts("/assets/precache-manifest.edb2b2ea191165e148bc4fa8d0364f27.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/*eslint-disable */
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+
+workbox.core.setCacheNameDetails({
+  prefix: 'puzzle',
+  precache: 'install-time',
+  suffix: 'v1',
+  runtime: 'run-time',
+  googleAnalytics: 'ga'
+});
+
+// active new service worker as long as it's installed
+workbox.clientsClaim();
+workbox.skipWaiting();
+
+// suppress warnings if revision is not provided
+workbox.precaching.suppressWarnings();
+
+// precahce and route asserts built by webpack
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+
+// routing for cloud served images
+workbox.routing.registerRoute(
+  /^https:\/\/.+\.(jpe?g|png|gif|svg|woff2|ico)$/i,
+  workbox.strategies.cacheFirst({
+    cacheName: 'puzzle-image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+        maxEntries: 20
+      }),
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+);
+
+/*eslint-enable */
+
