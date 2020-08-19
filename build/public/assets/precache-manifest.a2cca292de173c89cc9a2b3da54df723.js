51e8080badee8ad19cb4af36803482f0304bb933
Amit Jain <amitjain628@gmail.com>
2020-08-20 00:04:53 +0530

    Added changes

---
+++ build/public/assets/precache-manifest.a2cca292de173c89cc9a2b3da54df723.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce3a92b5acff3f6ca6f5",
+    "url": "/assets/vendors.ce3a92b5.js"
+  },
+  {
+    "revision": "770340b2b204624977e5",
+    "url": "/assets/home.770340b2.js"
+  },
+  {
+    "revision": "98d11e7cfeee0ee84fb0",
+    "url": "/assets/client.98d11e7c.js"
+  },
+  {
+    "revision": "7235a72566b6f271958dd7d32039aab3",
+    "url": "/app-shell"
+  }
+];
